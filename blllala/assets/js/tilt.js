9861f439184d489cc8ba77f2f30ec7bff5b7d6af
MUHAMMAD CELVIN <115867207+vnzyy@users.noreply.github.com>
2022-10-17 20:38:56 +0700

    Protoflio Muhammad Celvin
    :)

---
+++ blllala/assets/js/tilt.js
@@ -0,0 +1,5 @@
+$('.home__img').tilt({
+    scale: 1.2,
+    glare: true,
+    maxGlare: .5
+})
---
+++ blllala/assets/js/type.js
@@ -0,0 +1,6 @@
+var typed = new Typed('.animate', {
+    strings: ['Front End Developer', 'Youtuber', 'Gamer'],
+    backSpeed: 40,
+    typeSpeed: 80,
+    loop: true
+});
