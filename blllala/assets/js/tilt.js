86235d8aa4f433221b4bd12ba412457f542d7fb4
MUHAMMAD CELVIN <115867207+vnzyy@users.noreply.github.com>
2022-10-17 20:40:03 +0700

    Delete blllala directory

--- blllala/assets/js/tilt.js
+++
@@ -1,5 +0,0 @@
-$('.home__img').tilt({
-    scale: 1.2,
-    glare: true,
-    maxGlare: .5
-})
--- blllala/assets/js/type.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed('.animate', {
-    strings: ['Front End Developer', 'Youtuber', 'Gamer'],
-    backSpeed: 40,
-    typeSpeed: 80,
-    loop: true
-});
